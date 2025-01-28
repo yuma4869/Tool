f59057a8e8b3ea765f80024bf9c93793e3f2bedd
yuma4869 <tttyyy76@outlook.jp>
2025-01-28 17:40:59 +0900

    feature: add a mascot on HP

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,15 @@
 class IndexController < ApplicationController
     def base
     end
+
+    def mascot
+        $left_key = params[:left_key]
+        $right_key = params[:right_key]
+        $left_click = params[:left_click]
+        $right_click = params[:right_click]
+        $others = params[:others]
+        redirect_to "/"
+    end
     
     def about
     end
